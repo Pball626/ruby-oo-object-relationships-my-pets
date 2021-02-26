2e3e53a5fc4c00a38c51aaa1606113989b3371b2
joey <pball626@gmail.com>
2021-02-25 18:08:17 -0600

    commit tree 5a509a6d3d8ebe6f3b213b9fc131a6d80edff350

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,44 @@
-class Cat
-  # code goes here
+class Cat
+
+    attr_accessor :owner
+    attr_reader :name
+    @@all = []
+
+    def initialize(name, owner)
+      @name = name
+      @owner = owner
+      @@all << self 
+    end
+
+    def self.all
+      @@all
+    end
+  
 end
+# class Cat
+  
+#     attr_reader :name, :owner
+#     @@all = []
+  
+#   def initialize(name, owner)
+#     @name = name
+#     @owner = owner
+#     @@all << self
+#   end 
+
+#   # def owner=(owner)
+#   #   @owner
+#   # end
+
+#   def self.all
+#     @@all
+#   end
+
+#   # def name=(name)
+#   #   @name
+#   # end
+
+# end
+
+# # garfield = Cat.new("Garfield")
+# # Owner.garfield
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
+
 class Owner
-  # code goes here
+
+  attr_reader :name, :species, :count, :reset_all, :pet
+  @@all = []
+
+  def initialize(name)
+    # ^^^ why dont we have to add the rest of the att's to the argument
+    @name = name
+    @species = "human"
+    @count = count
+    @reset_all = reset_all
+    @pet = pet
+    @@all << self
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.reset_all
+    p @@all.clear
+  end
+
+  def buy_cat(cat_name)
+  end
+  
 end
+
+
+# victoria = Owner.new("Victoria")
+# victoria.say_species
+# # Owner.count
+
+# .all returns all instances of Owner that have been created
+# .reset_all can reset the owners that have been created (FAILED - 1)
